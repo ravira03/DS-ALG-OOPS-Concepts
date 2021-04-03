3121326fbfc80b9493bc9aa76c989846d554a666
ravira03 <ravivarmara22@gmail.com>
2021-04-03 11:19:10 +0530

    removing values from the stack using pop()

--- stack.py
+++ stack.py
@@ -11,6 +11,13 @@ class Stack:
         return self.size() == self.__elements
     def isEmpty(self):
         return self.size() == 0
+
+    def peek(self):
+        if self.isEmpty():
+            print("Stack has no element")
+            exit(1)
+        return self.__elements[self.__top]
+
     def push(self,value):
         if self.isFull():
             print("Stack is full")
@@ -19,9 +26,23 @@ class Stack:
         self.__top = self.__top + 1
         self.__elements[self.__top] = value
 
+    def pop(self):
+        if self.isEmpty():
+            print("Stack has no value")
+            exit(1)
+        print("Removing",self.peek(),"from stack")
+        top = self.__elements[self.__top]
+        self.__top = self.__top - 1
+        return top
+
 stack = Stack(5)
 stack.push("ravi")
 stack.push("varma")
 stack.push("gokull")
 stack.push("ganesh")
 stack.push("nandha")
+
+
+stack.pop()
+stack.pop()
+stack.pop()
