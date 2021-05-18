0f8a80767373fa966ee861c187ff8f17058aa21d
ravira03 <ravivarmara22@gmail.com>
2021-05-18 22:02:26 +0530

    Add Print in Reverse solution

---
+++ HackerRank_Problems/Print in Reverse.py
@@ -0,0 +1,17 @@
+def reversePrint(head):
+    if head:
+        reversePrint(head.next)
+        print(head.data)
+if __name__ == '__main__':
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        reversePrint(llist.head)
